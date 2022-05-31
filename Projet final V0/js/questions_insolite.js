e6eb32737a7a2cc123df9b30477ee3937da40671
ClaraDEPREZ <105223209+ClaraDEPREZ@users.noreply.github.com>
2022-05-31 15:33:13 +0200

    inso

--- Projet final V0/js/questions_insolite.js
+++ Projet final V0/js/questions_insolite.js
@@ -11,6 +11,7 @@ let questions = [
     "De voir un oiseau mort"
   ]
 },
+
   {
     type: 1,
   question: " Au Paraguay, les duels sont légaux à condition que les deux protagonistes... ",
@@ -27,21 +28,21 @@ let questions = [
   question: " Quel acte de la vie courante occupe eniron 6 mois de notre vie ? ",
   answer: "Attendre à un feu rouge",
   options: [
-    "passer le balais",
-    "regarder la télévision",
-    "sortir les poubelles",
+    "Passer le balais",
+    "Regarder la télévision",
+    "Sortir les poubelles",
     "Attendre à un feu rouge"
   ]
 },
   {
     type: 1,
   question: "Que s'est il passé le 25 décembre 1914 ?",
-  answer: " Noël ",
+  answer: "Noël",
   options: [
     "Le bombardement de Paris",
     "Le début de la première guerre mondiale",
     "Un appel par le Général de Gaulle",
-    " Noël "
+    "Noël"
   ]
 },
   {
@@ -55,6 +56,79 @@ let questions = [
     "La licorne"
   ]
 },
+
+{
+  type:2,
+  question:"Le 14 juillet existe au Royaume-Uni également.",
+  answer:"Vrai",
+  options: [
+    "Vrai",
+    "Faux"
+  ],
+  explanation: "Oui il existe, ce n'est juste pas le jour de la fête nationale comme en France",
+  },
+
+{
+    type:2,
+    question:"Il n'y a qu'un seul mois avec 28 jours.",
+    answer:"Faux",
+    options: [
+      "Vrai",
+      "Faux"
+    ],
+    explanation:"Tous les mois ont au moins 28 jours ! ",
+    },
+
+    {
+      type:1,
+      question:"Pendant combien de temps l'américain Charles Osborne a eu le hoquet?",
+      answer:"68 ans",
+      options: [
+        "10 minutes",
+        "2 ans",
+        "40 ans",
+        "68 ans"
+      ]
+      },
+
+{
+  type:1,
+  question:"Qu'est-ce que la natalophobie ?",
+  answer:"La peur de fêter Noël",
+  options: [
+    "La peur de la natation",
+    "La peur de fêter Noël",
+    "La peur des poupées",
+    "La peur de manger des nachos"
+  ],
+  explanation: "La natalophobie est la phobie de Noël, elle est considéré comme un trouble anxieux",
+        },
+
+{
+type:1,
+question:"Qu'est-ce que l'alektorophobie ?",
+answer:"La peur des poulets",
+options: [
+  "La peur de lire",
+  "La peur de manger des poires",
+  "La peur des poulets",
+  "La peur de dormir"
+]
+},
+
+{
+  type: 1,
+  question:"A votre avis, quel est le plus gros exportateur de chameaux au monde ?",
+  answer:"Norvège",
+  options: [
+   "Suisse",
+   "Norvège",
+   "L'Australie",
+   "La Tunisie" 
+  ],
+  explanation:" Le plus gros exportateur de chameaux du monde occidental est la Norvège.",
+  },
+
 ];
 
 /**
