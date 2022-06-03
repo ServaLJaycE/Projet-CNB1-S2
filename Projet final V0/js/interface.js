2cd65efee7846014858ef0be15906d6fd3f26454
BARTKOWIAKLouis <louis.bartkowiak@student.junia.com>
2022-06-03 10:57:55 +0200

    croix pour fermer pop-up

--- Projet final V0/js/interface.js
+++ Projet final V0/js/interface.js
@@ -1,6 +1,7 @@
 const score_box = document.querySelector(".score_box");
 const trophee = document.querySelector(".score button.trophy");
-const junia = document.querySelector(".junia")
+const quit_btn = document.querySelector(".score_box .quit_btn")
+const junia = document.querySelector(".junia");
 
 //trophée cliqué
 trophee.onclick = ()=>{
@@ -12,4 +13,10 @@ junia.onclick = ()=>{
     if ( confirm("Ceci va vous renvoyer vers le site JUNIA-Isen")){
         window.open("https://www.isen-lille.fr/formations/ecole-dingenieur-numerique-en-alternance/")
     }
+    alert("Ceci va vous renvoyer vers le site de JUNIA-ISEN")
+}
+
+//Croix cliquée
+quit_btn.onclick = () =>{
+    score_box.classList.remove("appear");
 }
